1c26c807cf16c51dada8ce83ac1d89517b5f52ea
agusval1994 <agusval1994@hotmail.com>
2018-03-29 20:28:35 -0300

    Primer cambio

--- app/src/main/java/com/example/agusv/pruebagithub/MainActivity.java
+++ app/src/main/java/com/example/agusv/pruebagithub/MainActivity.java
@@ -5,11 +5,11 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    int x = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
-
 }
